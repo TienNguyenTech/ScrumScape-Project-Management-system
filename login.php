eaacc626c8c27c2a7b0654092df1ae83d0bf5671
apat0080 <apat0080@student.monash.edu>
2024-09-02 23:06:21 +1000

    log in logic implemented

--- login.php
+++ login.php
@@ -1,97 +1,137 @@
 <?php
+ob_start();
+
 // Activate the session
 session_start();
+require_once('./database/dao.php');
+$dao = new DAO();
 
-global $dbh;
-require_once("connection.php");
+// Test if the user already logged in. If yes, send the user back to dashboard
+if (isset($_SESSION['user_id']))
+    header("Location: index.php");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!empty($_POST['username']) && !empty($_POST['password'])) {
-        // Check the user by username and password (plaintext matching)
-        $query = "SELECT * FROM `users` WHERE `username` = :username AND `password` = :password";
-        $stmt = $dbh->prepare($query);
+        var_dump($_POST['username'],$_POST['password'] );
+        $user = $dao->getUserByCredentials($_POST['username'], $_POST['password']); // hash the password, when getting a user
+        var_dump($user);
+        if ($user) {
+            $_SESSION['user_id'] = $user->user_email;
+            header("Location: home.php"); // go the index if user is logged in
+            exit();
 
-        if ($stmt->execute([
-                'username' => $_POST['username'],
-                'password' => $_POST['password']
-            ]) && $stmt->rowCount() == 1) {
-            // When the user is found, grab its id and store it into the session for future reference
-            $row = $stmt->fetchObject();
-            $_SESSION['user_id'] = $row->id;
-            //Successfully logged in, redirect user to dashboard
-            header("Location: index.php");
         } else {
-            $error_message = "Either username or password is incorrect!";
+            $alert_message = "Email or Password is incorrect"; // create alert var to display later on
+            $alert_type = "danger";
         }
+
     }
 }
-?>
 
+?>
 <!doctype html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Login</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <link rel="icon" href="../assets/logo-sm.png">
+    <link rel="stylesheet" href="./styles.css">
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
+        body, html {
+            height: 100%;
             margin: 0;
         }
-        .login-container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
-            max-width: 400px;
-            width: 100%;
+        .login-bg {
+            background-image: url("assets/login-bg.png");
+            height: 100%;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-size: cover;
+            z-index: -1;
         }
-        h1 {
-            text-align: center;
+        .heading {
+            font-family: "Montserrat", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 600;
+            font-style: normal;
+            color: var(--color-p);
+            font-size: 38px;
+         }
+        .sub-heading {
+            font-family: "Montserrat", sans-serif;
+            font-optical-sizing: auto;
+            font-weight: 200;
+            font-style: normal;
+            color: var(--color-s);
+            font-size: 20px;
         }
-        label {
-            display: block;
-            margin-bottom: 5px;
-            font-weight: bold;
-        }
-        input[type="text"], input[type="password"] {
-            width: 95%;
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #ccc;
-            border-radius: 5px;
-        }
-        input[type="submit"] {
-            width: 100%;
-            padding: 10px;
-            background-color: #28a745;
-            border: none;
-            color: white;
-            font-weight: bold;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-        h3.error {
-            color: red;
-            text-align: center;
+        .input {
+            background-color: #f1f1f1; font-weight: bolder; border: none; height: 50px; border-radius: 8px;
+            width: 300px;
         }
     </style>
+    <title>Login</title>
 </head>
 <body>
-<div class="login-container">
-    <h1>Log in</h1>
-    <h3 class="error"><?= $error_message ?? "" ?></h3>
-    <form method="post">
-        <label for="username">Username</label>
-        <input type="text" id="username" name="username" required>
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password" required>
-        <input type="submit" value="Login"/>
-    </form>
+<div class="login-bg">
+    <div class="shadow" style="background: white; height: 100%; width: 38%; z-index: 0; display: flex"">
+        <div  style=" margin-top: 40px; margin-left: 50px; z-index: 1; width: 100%">
+            <img src="assets/logo.png" style="width: 200px;"/>
+            <div style="margin-left: 50px; margin-top: 140px">
+                <h1 class="heading">Welcome back!</h1>
+                <p class="sub-heading">Ready to Scrum?</p>
+                <label for="username"></label>
+                <input type="text" class="form-control input" id="username" name="username" required placeholder="Username">
+                <label for="password"></label>
+                <input type="password" class="form-control input" id="password" name="password" required placeholder="Password">
+            </div>
+
+        </div>
+    </div>
+
+<!--    <div class="container mt-5 mb-5 custom-form">-->
+<!--        <div class="card glass-form">-->
+<!--            <div class="card-header">-->
+<!--                <h4 class="card-title">User Login</h4>-->
+<!--            </div>-->
+<!--            <div class="card-body">-->
+<!--                --><?php //if (isset($alert_message)): ?>
+<!--                    <div class="alert alert---><?php //= $alert_type ?><!--" role="alert">-->
+<!--                        --><?php //= $alert_message ?>
+<!--                    </div>-->
+<!--                --><?php //endif; ?>
+<!--                <form id="userLoginForm" method="POST">-->
+<!--                    <div class="form-group">-->
+<!--                        <label for="username">Username</label>-->
+<!--                        <input type="text" class="form-control" id="username" name="username" required>-->
+<!--                    </div>-->
+<!--                    <div class="form-group">-->
+<!--                        <label for="password">Password</label>-->
+<!--                        <input type="password" class="form-control" id="password" name="password" required>-->
+<!--                    </div>-->
+<!--                    <div class="mt-3 d-flex justify-content-between">-->
+<!--                        <button type="submit" value="Login" class="btn btn-primary">Login</button>-->
+<!--                    </div>-->
+<!--                </form>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
 </div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6jJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
 </html>
