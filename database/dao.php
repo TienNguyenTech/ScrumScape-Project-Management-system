9cb282dfe0dde5819583a00d59fdf953c84189e8
0sirota <osir0002@student.monash.edu>
2024-09-04 19:24:24 +1000

    Deleted debugging statements.

--- database/dao.php
+++ database/dao.php
@@ -52,8 +52,6 @@ class dao
             $this->_query = "UPDATE `USER` SET `user_password` = SHA2(?, 256) WHERE `user_name`  = ?";
             $this->_stmt = $this->_db_handle->prepare($this->_query);
             $this->_stmt->execute([$password, $username]);
-            var_dump($username);
-            var_dump($password);
             return true;
         } catch (Exception $e) {
             $this->_error = $e->getMessage();
