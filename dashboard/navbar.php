f486133bcbf8b7be31122d3fdfa8ffd958fab848
apat0080 <apat0080@student.monash.edu>
2024-09-09 14:16:17 +1000

    implemented side bar

--- dashboard/navbar.php
+++ dashboard/navbar.php
@@ -6,82 +6,141 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- Bootstrap link-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    
+
     <!-- Fonts links -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+
     <style>
         body {
             background-color: #F3EFEF;
         }
+
         .drop-shadow {
             box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
         }
 
         .navbar-toggler img {
-            width:20px;
-            height:20px
+            width: 30px;
+            height: 30px;
         }
 
-        .navbar-toggler {
-            border-width: 0px
-        }
 
-        .admin-card *{
+        .admin-card * {
             color: black;
             font-family: "Montserrat", sans-serif;
             margin: 8px;
         }
 
-        .sublinks {
-            font-size: 13px;
-        }
 
         .admin-card a:hover {
-            color: grey
+            color: grey;
         }
 
-        .admin-card {
-            border-radius: 20px;
-            margin: 80px;
-        }
 
         .sublinks img {
             width: 15px;
             height: 15px;
         }
+
+        /* Sidebar */
+        .sidebar {
+            height: 100%;
+            width: 0;
+            position: fixed;
+            z-index: 1;
+            top: 0;
+            right: 0;
+            background-color: #fff;
+            overflow-x: hidden;
+            transition: 0.5s;
+            box-shadow: -2px 0 5px rgba(0, 0, 0, 0.5);
+        }
+
+        .sidebar a {
+            padding: 15px 30px;
+            text-decoration: none;
+            font-size: 19px;
+            color: #111;
+            display: block;
+            transition: 0.3s;
+        }
+
+        .sidebar a:hover {
+            color: #f1f1f1;
+            background-color: grey;
+        }
+
+        .sidebar .closebtn {
+            position: absolute;
+            top: 10px;
+            left: 25px;
+            font-size: 36px;
+        }
+
+        img {
+            border: none;
+            outline: none;
+        }
     </style>
 </head>
 
 <body>
-    <!-- Collapsible sidebar -->
-    <div class="nav-flex-column">
-        <div class="collapse" id="navbarToggleExternalContent">
-          <div class="bg-white p-4">
-            <h4 class="text-dark">Menu</h4>
-            <button class="btn btn-primary"> Option 1 </button>
-            <button class="btn btn-primary"> Option 2 </button>
-            <button class="btn btn-primary"> Option 3 </button>
-          </div>
-        </div>
+<!-- Navigation bar -->
+<nav class="navbar navbar-light bg-white drop-shadow d-flex justify-content-between align-items-center">
+    <a class="navbar-brand" href="#">
+        <img src="../assets/logo.png" style="width: 150px;" class="d-inline-block align-top" alt="logo">
+    </a>
+    <!-- Hamburger button -->
+    <div style="border: none" id="hamburger-btn" class="navbar-toggler mt-2 mr-3" onclick="openNav()">
+        <img src="../assets/ham.png" alt="Menu" class="img-fluid navbar-toggle">
     </div>
-    
-    <!-- Navigation bar -->
-    <nav class="navbar navbar-light bg-white drop-shadow container-fluid-m3">
-        <a class="navbar-brand" href="#">
-            <img src="../assets/logo.png" style="width: 130px;" class="d-inline-block align-top" alt="logo">
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-            <img src="../assets/dropdown.svg">
-        </button>
-    </nav>
+</nav>
 
-    <!-- Bootstrap optional javascript-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+<!-- Sidebar -->
+<div id="mySidebar" class="sidebar">
+    <br>
+
+    <a href="/dashboard">Dashboard</a>
+    <br>
+    <a href="#">All Sprints</a>
+    <a href="#">Add Sprints</a>
+    <br>
+    <a href="#">All Team Members</a>
+    <a href="#">Add Team Members</a>
+    <br>
+
+
+</div>
+
+<!-- Bootstrap optional javascript-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7HfUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<!-- Sidebar toggle scripts -->
+<script>
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "250px";
+    }
+
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+    }
+
+    // Close sidebar if clicked outside of it
+    document.addEventListener('click', function(event) {
+        var sidebar = document.getElementById('mySidebar');
+        var hamburgerBtn = document.getElementById('hamburger-btn');
+
+        if (!sidebar.contains(event.target) && !hamburgerBtn.contains(event.target)) {
+            closeNav();
+        }
+    });
+</script>
 </body>
+
 </html>
