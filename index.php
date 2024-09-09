e150d0f525cb0655eeba1a2235e795083e5b0708
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-09-09 15:35:23 +1000

    Updated the links on index

--- index.php
+++ index.php
@@ -12,14 +12,11 @@
     </style>
 </head>
 <body>
-<p><a href="./students1/index.php">Students1</a></p>
-<p><a href="./schools/index.php">Schools</a></p>
-<p><a href="./courses/index.php">Courses</a></p>
-<p><a href="./students/index.php">Students</a></p>
+<h1>Hello This is the homepage</h1>
 <p><a href="./sprints/index.php">Sprints</a></p>
 <p><a href="./tasks/index.php">Tasks</a></p>
 <p><a href="./time_logs/index.php">Time Logs</a></p>
-<h1>Hello This is the homepage</h1>
+
 <hr>
 <p><a href="login.php">Login</a></p>
 <p><a href="logout.php">Logout</a></p>
