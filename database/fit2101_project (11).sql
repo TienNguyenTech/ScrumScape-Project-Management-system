8e09cb51f7f802be6ba76bda4603efcc340375c7
Tien Nhat Nguyen <tngu0394@student.monash.edu>
2024-09-19 18:13:57 +1000

    Updated new schema

--- database/fit2101_project (11).sql
+++ database/fit2101_project (11).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 19, 2024 at 06:14 AM
+-- Generation Time: Sep 19, 2024 at 09:21 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,45 +24,44 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sprints`
+-- Table structure for table `sprint`
 --
 
-CREATE TABLE `sprints` (
+CREATE TABLE `sprint` (
   `sprint_id` int(11) NOT NULL,
   `sprint_no` int(11) NOT NULL,
   `sprint_name` varchar(255) NOT NULL,
   `start_date` date NOT NULL,
   `end_date` date NOT NULL,
   `status` enum('Not Started','In Progress','Completed') DEFAULT 'Not Started',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `duration` int(11) DEFAULT NULL
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `sprints`
+-- Dumping data for table `sprint`
 --
 
-INSERT INTO `sprints` (`sprint_id`, `sprint_no`, `sprint_name`, `start_date`, `end_date`, `status`, `created_at`, `duration`) VALUES
-(1, 1, 'Login & Homepage Development', '2024-09-11', '2024-10-02', 'Completed', '2024-09-10 23:30:00', 14),
-(2, 2, 'Tasks & Sprints Development', '2024-10-03', '2024-10-16', 'In Progress', '2024-10-02 23:30:00', 14),
-(3, 3, 'Kanban Board & Burndown Chart Development', '2024-10-17', '2024-10-30', 'Not Started', '2024-10-16 22:30:00', 14);
+INSERT INTO `sprint` (`sprint_id`, `sprint_no`, `sprint_name`, `start_date`, `end_date`, `status`, `created_at`) VALUES
+(1, 1, 'Login & Homepage Development', '2024-09-11', '2024-10-02', 'Completed', '2024-09-10 23:30:00'),
+(2, 2, 'Tasks & Sprints Development', '2024-10-03', '2024-10-16', 'In Progress', '2024-10-02 23:30:00'),
+(3, 3, 'Kanban Board & Burndown Chart Development', '2024-10-17', '2024-10-30', 'Not Started', '2024-10-16 22:30:00');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sprint_assignments`
+-- Table structure for table `sprint_assignment`
 --
 
-CREATE TABLE `sprint_assignments` (
+CREATE TABLE `sprint_assignment` (
   `sprint_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `sprint_assignments`
+-- Dumping data for table `sprint_assignment`
 --
 
-INSERT INTO `sprint_assignments` (`sprint_id`, `user_id`) VALUES
+INSERT INTO `sprint_assignment` (`sprint_id`, `user_id`) VALUES
 (1, 3),
 (1, 4),
 (1, 5),
@@ -77,10 +76,10 @@ INSERT INTO `sprint_assignments` (`sprint_id`, `user_id`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tasks`
+-- Table structure for table `task`
 --
 
-CREATE TABLE `tasks` (
+CREATE TABLE `task` (
   `task_id` int(11) NOT NULL,
   `task_no` int(11) NOT NULL,
   `task_name` varchar(255) NOT NULL,
@@ -91,10 +90,10 @@ CREATE TABLE `tasks` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `tasks`
+-- Dumping data for table `task`
 --
 
-INSERT INTO `tasks` (`task_id`, `task_no`, `task_name`, `story_points`, `priority`, `status`, `created_at`) VALUES
+INSERT INTO `task` (`task_id`, `task_no`, `task_name`, `story_points`, `priority`, `status`, `created_at`) VALUES
 (1, 1, 'Create Task: Backend DAO function', 5.00, 'Medium', 'Completed', '2024-09-17 17:23:07'),
 (2, 2, 'Create Task: UI task registration form', 3.00, 'Medium', 'Completed', '2024-09-17 17:23:07'),
 (3, 3, 'Update Task: Backend DAO function', 4.00, 'Medium', 'In Progress', '2024-09-17 17:23:07'),
@@ -117,20 +116,20 @@ INSERT INTO `tasks` (`task_id`, `task_no`, `task_name`, `story_points`, `priorit
 -- --------------------------------------------------------
 
 --
--- Table structure for table `task_assignments`
+-- Table structure for table `task_assignment`
 --
 
-CREATE TABLE `task_assignments` (
+CREATE TABLE `task_assignment` (
   `task_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `logged_hours` decimal(5,2) DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `task_assignments`
+-- Dumping data for table `task_assignment`
 --
 
-INSERT INTO `task_assignments` (`task_id`, `user_id`, `logged_hours`) VALUES
+INSERT INTO `task_assignment` (`task_id`, `user_id`, `logged_hours`) VALUES
 (1, 3, 5.00),
 (2, 4, 4.50),
 (3, 5, 3.25),
@@ -153,10 +152,10 @@ INSERT INTO `task_assignments` (`task_id`, `user_id`, `logged_hours`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `user_id` int(11) NOT NULL,
   `user_email` varchar(255) NOT NULL,
   `user_name` varchar(64) NOT NULL,
@@ -167,10 +166,10 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `users` (`user_id`, `user_email`, `user_name`, `user_password`, `user_fname`, `user_lname`, `admin`) VALUES
+INSERT INTO `user` (`user_id`, `user_email`, `user_name`, `user_password`, `user_fname`, `user_lname`, `admin`) VALUES
 (1, 'achini.jayawardane@monash.edu', 'admin', '41e5653fc7aeb894026d6bb7b2db7f65902b454945fa8fd65a6327047b5277fb', 'Achini', 'Jayawardane', 1),
 (2, 'nisal.desilva1@monash.edu', 'lecturer', '0098b8d8dca520e950652311b7e89780bf3a3b9702c9aadeacb3977b7ebca78e', 'Nisal', 'de Silva', 1),
 (3, 'velocity.admin@velocitycode.com', 'velocity', 'a47b08aabb73a27f192c1fa25f232b61f3da4c251e3e604724630ca3ef0bb6e8', 'Velocity', 'Admin', 1),
@@ -186,35 +185,35 @@ INSERT INTO `users` (`user_id`, `user_email`, `user_name`, `user_password`, `use
 --
 
 --
--- Indexes for table `sprints`
+-- Indexes for table `sprint`
 --
-ALTER TABLE `sprints`
+ALTER TABLE `sprint`
   ADD PRIMARY KEY (`sprint_id`);
 
 --
--- Indexes for table `sprint_assignments`
+-- Indexes for table `sprint_assignment`
 --
-ALTER TABLE `sprint_assignments`
+ALTER TABLE `sprint_assignment`
   ADD PRIMARY KEY (`sprint_id`,`user_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `tasks`
+-- Indexes for table `task`
 --
-ALTER TABLE `tasks`
+ALTER TABLE `task`
   ADD PRIMARY KEY (`task_id`);
 
 --
--- Indexes for table `task_assignments`
+-- Indexes for table `task_assignment`
 --
-ALTER TABLE `task_assignments`
+ALTER TABLE `task_assignment`
   ADD PRIMARY KEY (`task_id`,`user_id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `user`
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `user_email` (`user_email`),
   ADD UNIQUE KEY `user_name` (`user_name`);
@@ -224,21 +223,21 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `sprints`
+-- AUTO_INCREMENT for table `sprint`
 --
-ALTER TABLE `sprints`
+ALTER TABLE `sprint`
   MODIFY `sprint_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `tasks`
+-- AUTO_INCREMENT for table `task`
 --
-ALTER TABLE `tasks`
+ALTER TABLE `task`
   MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
@@ -246,18 +245,18 @@ ALTER TABLE `users`
 --
 
 --
--- Constraints for table `sprint_assignments`
+-- Constraints for table `sprint_assignment`
 --
-ALTER TABLE `sprint_assignments`
-  ADD CONSTRAINT `sprint_assignments_ibfk_1` FOREIGN KEY (`sprint_id`) REFERENCES `sprints` (`sprint_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `sprint_assignments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+ALTER TABLE `sprint_assignment`
+  ADD CONSTRAINT `sprint_assignment_ibfk_1` FOREIGN KEY (`sprint_id`) REFERENCES `sprint` (`sprint_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `sprint_assignment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 
 --
--- Constraints for table `task_assignments`
+-- Constraints for table `task_assignment`
 --
-ALTER TABLE `task_assignments`
-  ADD CONSTRAINT `task_assignments_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `tasks` (`task_id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `task_assignments_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
+ALTER TABLE `task_assignment`
+  ADD CONSTRAINT `task_assignment_ibfk_1` FOREIGN KEY (`task_id`) REFERENCES `task` (`task_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `task_assignment_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
