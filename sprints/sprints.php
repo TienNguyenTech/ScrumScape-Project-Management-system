c5313845e2b789de94f7eb1c7d9d7353a570b55d
0sirota <osir0002@student.monash.edu>
2024-10-02 18:23:47 +1000

    Changed update_sprint.php in sprints to updatesprint.html

--- sprints/sprints.php
+++ sprints/sprints.php
@@ -304,7 +304,7 @@ function formatDate($date) {
                 </td>
                 <td class="d-flex align-items-center table-cell justify-content-center" style="border: none; gap: 4px">
                     <!-- Edit button -->
-                    <a href="update_sprint.php">
+                    <a href="../backlog/updatesprint.html">
                         <button type="button" class="btn btn-outline-primary" style="padding:7px; border: none;" onclick="event.stopPropagation();">
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
