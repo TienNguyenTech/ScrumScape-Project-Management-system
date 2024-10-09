4ff61c94a20658be743514cd859cdb3fbe513c4a
dpab0001 <dannapabayo@gmail.com>
2024-10-09 14:22:44 +1100

    Completed updated create task page for backlog

--- backlog/create_task_updated.php
+++ backlog/create_task_updated.php
@@ -39,7 +39,6 @@
             color: #6F7482;
             font-family: "Montserrat"
             margin-bottom: 100px;
-            font-weight: bold;
         }
 
         .custom-btn {
@@ -64,7 +63,7 @@
             font-family: "Montserrat";
             color: #6F7482;
             font-weight: light;
-            font-size: 13px;
+            font-size: 15px;
         }
         
         .sub-field {
@@ -77,7 +76,23 @@
             height:20%;
         }
 
-        
+        .tags {
+            display: flex;
+            flex-direction: column;
+            gap:13px;
+            width: 50%;
+            padding: 20px 0px 0px 50px;
+        }
+
+        .tag {
+            margin-left: 10px;
+            padding: 8px;
+            border-radius: 10px;
+            font-family: "Montserrat";
+            font-weight: 0.9;
+            color: white;
+            font-size: 12px;
+        }
     </style>
 
 </head>
@@ -109,11 +124,14 @@
                 </div>
             </div>
 
+            <!-- Side Panel -->
+            <!-- Note: Don't add background color for dropdowns, otherwise might affect human aspect part of project.-->
             <div style="background-color: #F4F3F3; width: 25%;">
 
+                <!-- Task Type -->
                 <div class="info-section">
                     <div class="sub-header">
-                        <img src="../assets/task_icon.svg" style="width:15px; color: #6F7482;"/>    
+                        <img src="../assets/task_icon_grey.svg" style="width:15px; color: #6F7482; padding-bottom: 8px;"/>    
                         <h4> Task Type </h4>
                     </div>
                     <select class="form-control form-control-sm sub-field main-field">
@@ -123,9 +141,10 @@
                     <hr style="border: 1px solid #AEA8A8; width: 50%; margin: 20px auto;">
                 </div>
 
+                <!-- Story Points -->
                 <div class="info-section">
                     <div class="sub-header">
-                        <img src="../assets/task_icon.svg" style="width:15px; color: #6F7482;"/>    
+                        <img src="../assets/storypt_icon_grey.svg" style="width:18px; color: #6F7482; padding-bottom: 8px;"/>    
                         <h4> Story Points </h4>
                     </div>
                     <select class="form-control form-control-sm sub-field main-field">
@@ -143,9 +162,10 @@
                     <hr style="border: 1px solid #AEA8A8; width: 50%; margin: 20px auto;">
                 </div>
 
+                <!-- Priority -->
                 <div class="info-section">
                     <div class="sub-header">
-                        <img src="../assets/task_icon.svg" style="width:15px; color: #6F7482;"/>    
+                        <img src="../assets/priority_icon_grey.svg" style="width:14px; color: #6F7482; padding-bottom: 8px;"/>    
                         <h4> Priority </h4>
                     </div>
                     <select class="form-control form-control-sm sub-field main-field">
@@ -157,7 +177,54 @@
                     <hr style="border: 1px solid #AEA8A8; width: 50%; margin: 20px auto;">
                 </div>
 
-                <div class="info-section-tags" style="background-color: red; ">
+                <!-- Tags -->
+                <div class="info-section-tags" style="height: 40%;">
+                    <div class="sub-header">
+                        <img src="../assets/tags_icon.svg" style="width:18px; color: #6F7482; padding-bottom: 8px;"/>    
+                        <h4> Tags </h4>
+                    </div>
+                    
+                    <!-- Don't change colours, picked specifically for colour blind aspect.-->
+                    <div style="display:flex;">
+                        <div class="tags">
+                            <div class="form-check">
+                                <input class="form-check-input position-static custom-check" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #7F74F6;"> Frontend </span>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #7F74F6;"> Backend </span>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #E34F9F"> API </span>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #E34F9F"> Database </span>
+                            </div>
+                        </div>
+
+                        <div class="tags">
+                            <div class="form-check">
+                                <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #E34F9F"> Framework </span>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #E34F9F"> Testing </span>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #E1982A"> UI </span>
+                            </div>
+                            <div class="form-check">
+                                <input class="form-check-input position-static" type="checkbox" id="blankCheckbox" value="option1" aria-label="...">
+                                <span class="tag" style="background-color: #E1982A"> UX </span>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             
             </div>
